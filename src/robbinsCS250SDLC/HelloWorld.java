7ea740e859c746adee1240fe68c0166bf9930140
NihilKri <nehilkori@gmail.com>
2024-10-23 23:46:21 -0500

    Added URL that helped me to set up github

--- src/robbinsCS250SDLC/HelloWorld.java
+++ src/robbinsCS250SDLC/HelloWorld.java
@@ -14,6 +14,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello CS-250!");
+		
+		// http://makeseleniumeasy.com/2023/05/17/how-to-push-code-from-eclipse-to-github-repo-using-access-token/
 	}
 
 }
